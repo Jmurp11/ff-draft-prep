3312f57f3ad6be1ac9a919f0bdac96fee803e244
Jim Murphy <murphyj1011@gmail.com>
2019-08-11 16:52:15 -0400

    add client components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PlayerTableComponent } from './player-table/player-table.component';
+import { TeamComponent } from './team/team.component';
+import { LeagueFormComponent } from './league-form/league-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PlayerTableComponent,
+    TeamComponent,
+    LeagueFormComponent
   ],
   imports: [
     BrowserModule,
