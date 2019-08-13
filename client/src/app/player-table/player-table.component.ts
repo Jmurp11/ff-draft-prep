e913b97a94f01d51d4379db6fd5db2983812a408
Murphy <jim.murphy@nera.com>
2019-08-13 17:19:50 -0400

    add angular code

--- client/src/app/player-table/player-table.component.ts
+++ client/src/app/player-table/player-table.component.ts
@@ -1,4 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PlayerData {
+  playerName: string;
+  team: string;
+  position: string;
+  completions: number;
+  attempts: number;
+  passYards: number;
+  passTd: number;
+  interception: number;
+  carries: number;
+  rushYards: number;
+  rushTd: number;
+  fumbles: number;
+  targets: number;
+  receptions: number;
+  receivingYards: number;
+  receivingTd: number;
+}
+
+/** Constants used to fill up our data base. */
+const TEAMS: string[] = [
+  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
+  'aqua', 'blue', 'navy', 'black', 'gray'
+];
+const NAMES: string[] = [
+  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
+
+const POSITIONS: string[] = [
+  'QB', 'RB', 'WR', 'TE'
+];
 
 @Component({
   selector: 'app-player-table',
@@ -7,9 +43,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerTableComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'playerName',
+    'team',
+    'position',
+    'passAC',
+    'passYards',
+    'passTd',
+    'interception',
+    'carries',
+    'rushYards',
+    'rushTd',
+    'fumbles',
+    'receptions',
+    'targets',
+    'receivingTd',
+    'receivingYards',
+  ];
+  dataSource: MatTableDataSource<PlayerData>;
 
-  ngOnInit() {
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor() {
+    // Create 100 users
+    const users = Array.from({ length: 100 }, (_, k) => createPlayer(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
   }
 
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+/** Builds and returns a new User. */
+function createPlayer(id: number): PlayerData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    playerName: name,
+    team: TEAMS[Math.round(Math.random() * (TEAMS.length - 1))],
+    position: POSITIONS[Math.round(Math.random() * (POSITIONS.length - 1))],
+    completions: 0,
+    attempts: 0,
+    passYards: 0,
+    passTd: 0,
+    interception: 0,
+    carries: 0,
+    rushYards: 0,
+    rushTd: 0,
+    fumbles: 0,
+    receptions: 0,
+    targets: 0,
+    receivingTd: 0,
+    receivingYards: 0
+  };
 }
