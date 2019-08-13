58c982cc1ed7a206771628fdcba76b4e19e9a610
Murphy <jim.murphy@nera.com>
2019-08-13 16:39:34 -0400

    add angular code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PlayerTableComponent } from './player-table/player-table.component';
 import { TeamComponent } from './team/team.component';
 import { LeagueFormComponent } from './league-form/league-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +20,17 @@ import { LeagueFormComponent } from './league-form/league-form.component';
     NavbarComponent,
     PlayerTableComponent,
     TeamComponent,
-    LeagueFormComponent
+    LeagueFormComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
