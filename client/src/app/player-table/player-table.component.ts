99018442440fc8094cd993cf3d5a29a167dea119
Murphy <jim.murphy@nera.com>
2019-08-20 22:57:21 -0400

    add expandable table.  format description section. to do: add images to the server

--- client/src/app/player-table/player-table.component.ts
+++ client/src/app/player-table/player-table.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 export interface PlayerData {
   playerName: string;
@@ -24,21 +24,24 @@ export interface PlayerData {
   receptions: number;
   receivingYards: number;
   receivingTd: number;
+  pointsFor: number;
+  yards: number;
+  plays: number;
+  yardsPerPlay: number;
+  turnovers: number;
+  passAttempts: number;
+  passCompletions: number;
+  passYardsTeam: number;
+  passTdTeam: number;
+  interceptionTeam: number;
+  netYardsPerPass: number;
+  rushAttemptTeam: number;
+  rushYardsTeam: number;
+  rushTdTeam: number;
+  yardsPerRush: number;
+  scorePercentage: number;
+  turnoverPercentage: number;
 }
-
-/** Constants used to fill up our data base. */
-const TEAMS: string[] = [
-  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
-  'aqua', 'blue', 'navy', 'black', 'gray'
-];
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
-
-const POSITIONS: string[] = [
-  'QB', 'RB', 'WR', 'TE'
-];
 
 @Component({
   selector: 'app-player-table',
@@ -46,46 +49,27 @@ const POSITIONS: string[] = [
   styleUrls: ['./player-table.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
 
 export class PlayerTableComponent implements OnInit {
-
-  displayedColumns: string[] = [
-    'rank',
-    'playerName',
-    'position',
-    'tier',
-    'team',
-    'bye',
-    'passAC',
-    'passYards',
-    'passTd',
-    'interception',
-    'carries',
-    'rushYards',
-    'rushTd',
-    'fumbles',
-    'targets',
-    'receptions',
-    'receivingYards',
-    'receivingTd',
-  ];
   dataSource: MatTableDataSource<PlayerData>;
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  constructor() {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createPlayer(k + 1));
+  columnsToDisplay = ['rank', 'playerName', 'team', 'position', 'tier', 'completions', 'attempts',
+    'passYards', 'passTd', 'interception', 'carries', 'rushYards', 'rushTd', 'fumbles', 'targets', 'receptions',
+    'receivingYards', 'receivingTd'];
+  expandedElement: PlayerData | null;
 
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+
+  constructor() {
+    this.dataSource = new MatTableDataSource(PLAYER_DATA);
   }
 
   ngOnInit() {
@@ -102,15 +86,14 @@ export class PlayerTableComponent implements OnInit {
   }
 }
 
-/** Builds and returns a new User. */
-function createPlayer(id: number): PlayerData {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    playerName: name,
-    team: TEAMS[Math.round(Math.random() * (TEAMS.length - 1))],
-    position: POSITIONS[Math.round(Math.random() * (POSITIONS.length - 1))],
+const PLAYER_DATA: PlayerData[] = [
+  {
+    playerName: 'Leveon Bell',
+    team: 'NYJ',
+    position: 'RB',
+    rank: 1,
+    tier: 2,
+    bye: 11,
     completions: 0,
     attempts: 0,
     passYards: 0,
@@ -120,12 +103,174 @@ function createPlayer(id: number): PlayerData {
     rushYards: 0,
     rushTd: 0,
     fumbles: 0,
-    receptions: 0,
     targets: 0,
-    receivingTd: 0,
+    receptions: 0,
     receivingYards: 0,
-    rank: Math.round(Math.random() * 500),
-    tier: Math.round(Math.random() * 11),
-    bye: Math.round(Math.random() * 16),
-  };
-}
+    receivingTd: 0,
+    pointsFor: 0,
+    yards: 0,
+    plays: 0,
+    yardsPerPlay: 0.0,
+    turnovers: 0,
+    passAttempts: 0,
+    passCompletions: 0,
+    passYardsTeam: 0,
+    passTdTeam: 0,
+    interceptionTeam: 0,
+    netYardsPerPass: 0.0,
+    rushAttemptTeam: 0,
+    rushYardsTeam: 0,
+    rushTdTeam: 0,
+    yardsPerRush: 0,
+    scorePercentage: 0.0,
+    turnoverPercentage: 0.0
+  }, {
+    playerName: 'Todd Gurley',
+    team: 'LAR',
+    position: 'RB',
+    rank: 2,
+    tier: 2,
+    bye: 11,
+    completions: 0,
+    attempts: 0,
+    passYards: 0,
+    passTd: 0,
+    interception: 0,
+    carries: 0,
+    rushYards: 0,
+    rushTd: 0,
+    fumbles: 0,
+    targets: 0,
+    receptions: 0,
+    receivingYards: 0,
+    receivingTd: 0,
+    pointsFor: 0,
+    yards: 0,
+    plays: 0,
+    yardsPerPlay: 0.0,
+    turnovers: 0,
+    passAttempts: 0,
+    passCompletions: 0,
+    passYardsTeam: 0,
+    passTdTeam: 0,
+    interceptionTeam: 0,
+    netYardsPerPass: 0.0,
+    rushAttemptTeam: 0,
+    rushYardsTeam: 0,
+    rushTdTeam: 0,
+    yardsPerRush: 0,
+    scorePercentage: 0.0,
+    turnoverPercentage: 0.0
+  }, {
+    playerName: 'Mike Davis',
+    team: 'CHI',
+    position: 'RB',
+    rank: 3,
+    tier: 2,
+    bye: 11,
+    completions: 0,
+    attempts: 0,
+    passYards: 0,
+    passTd: 0,
+    interception: 0,
+    carries: 0,
+    rushYards: 0,
+    rushTd: 0,
+    fumbles: 0,
+    targets: 0,
+    receptions: 0,
+    receivingYards: 0,
+    receivingTd: 0,
+    pointsFor: 0,
+    yards: 0,
+    plays: 0,
+    yardsPerPlay: 0.0,
+    turnovers: 0,
+    passAttempts: 0,
+    passCompletions: 0,
+    passYardsTeam: 0,
+    passTdTeam: 0,
+    interceptionTeam: 0,
+    netYardsPerPass: 0.0,
+    rushAttemptTeam: 0,
+    rushYardsTeam: 0,
+    rushTdTeam: 0,
+    yardsPerRush: 0,
+    scorePercentage: 0.0,
+    turnoverPercentage: 0.0
+  }, {
+    playerName: 'Patrick Mahomes',
+    team: 'KC',
+    position: 'QB',
+    rank: 4,
+    tier: 2,
+    bye: 11,
+    completions: 0,
+    attempts: 0,
+    passYards: 0,
+    passTd: 0,
+    interception: 0,
+    carries: 0,
+    rushYards: 0,
+    rushTd: 0,
+    fumbles: 0,
+    targets: 0,
+    receptions: 0,
+    receivingYards: 0,
+    receivingTd: 0,
+    pointsFor: 0,
+    yards: 0,
+    plays: 0,
+    yardsPerPlay: 0.0,
+    turnovers: 0,
+    passAttempts: 0,
+    passCompletions: 0,
+    passYardsTeam: 0,
+    passTdTeam: 0,
+    interceptionTeam: 0,
+    netYardsPerPass: 0.0,
+    rushAttemptTeam: 0,
+    rushYardsTeam: 0,
+    rushTdTeam: 0,
+    yardsPerRush: 0,
+    scorePercentage: 0.0,
+    turnoverPercentage: 0.0
+  }, {
+    playerName: 'Mike Evans',
+    team: 'TB',
+    position: 'WR',
+    rank: 5,
+    tier: 2,
+    bye: 11,
+    completions: 0,
+    attempts: 0,
+    passYards: 0,
+    passTd: 0,
+    interception: 0,
+    carries: 0,
+    rushYards: 0,
+    rushTd: 0,
+    fumbles: 0,
+    targets: 0,
+    receptions: 0,
+    receivingYards: 0,
+    receivingTd: 0,
+    pointsFor: 0,
+    yards: 0,
+    plays: 0,
+    yardsPerPlay: 0.0,
+    turnovers: 0,
+    passAttempts: 0,
+    passCompletions: 0,
+    passYardsTeam: 0,
+    passTdTeam: 0,
+    interceptionTeam: 0,
+    netYardsPerPass: 0.0,
+    rushAttemptTeam: 0,
+    rushYardsTeam: 0,
+    rushTdTeam: 0,
+    yardsPerRush: 0,
+    scorePercentage: 0.0,
+    turnoverPercentage: 0.0
+  }
+];
---
+++ client/src/app/shared/images.ts
@@ -0,0 +1,10 @@
+export const imageUrls = [
+  {
+    team: 'NYJ',
+    url: 'http://content.sportslogos.net/logos/7/152/thumbs/15291162019.gif'
+  },
+  {
+    team: 'CHI',
+    url: 'http://content.sportslogos.net/logos/7/169/thumbs/364.gif'
+  },
+];
