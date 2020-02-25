7a5296ec08a54644ec896431a5264624ad48244d
Jim Murphy <murphyj1011@gmail.com>
2020-02-25 08:45:19 -0500

    client-side changes

--- client/src/app/note-card/note-card.component.ts
+++ client/src/app/note-card/note-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Apollo } from 'apollo-angular';
+import { Player } from '../player-table/Player';
+import { PlayerService } from '../player-table/player.service';
 
 @Component({
   selector: 'app-note-card',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-card.component.css']
 })
 export class NoteCardComponent implements OnInit {
-  
-  constructor() { }
+  _currentPlayer: Subscription;
+  currentPlayer: Player;
+  backgroundColor: string;
+
+  constructor(
+    private _player: PlayerService,
+    private apollo: Apollo
+  ) { }
 
   ngOnInit() {
+    this._currentPlayer = this._player.currentPlayer.subscribe(data => {
+      this.currentPlayer = data;
+      switch (this.currentPlayer.player.position) {
+        case 'QB':
+          this.backgroundColor = 'lightskyblue';
+          break;
+        case 'RB':
+          this.backgroundColor = 'lightgreen';
+          break;
+        case 'WR':
+          this.backgroundColor = 'lightpink';
+          break;
+        case 'TE':
+          this.backgroundColor = 'lightgoldenrodyellow';
+          break;
+      }
+    });
   }
 
+  notesQuery(player: number) {
+
+  }
+  ngOnDestroy() {
+    this._currentPlayer.unsubscribe();
+  }
 }
