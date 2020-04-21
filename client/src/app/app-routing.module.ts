c1a26914e6296ecdb954a754de516eefcf4eddd2
Murphy <jim.murphy@nera.com>
2020-04-21 15:39:03 -0400

    update styling

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth/auth.guard';
-import { PlayerTableComponent } from './draft/player/player-table/player-table.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
